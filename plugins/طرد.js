3132bf1e5ab9af92497d5b75a84bf1ce35306c42
Hussein Ali <112989978+Gonfreks@users.noreply.github.com>
2024-08-29 02:59:11 +0300

    Update طرد.js

--- plugins/طرد.js
+++ plugins/طرد.js
@@ -1,21 +1,38 @@
+ let handler = async (m, { conn, participants, usedPrefix, command }) => {
 
-let handler = async (m, { conn, participants, usedPrefix, command }) => {
+    let developers = ['201146121794@s.whatsapp.net'];   
 
-let kickte = `*مــنشـن الـشـخص !*`
+    let kickte = `*مــنشـن الـشـخص !*`;
 
-if (!m.mentionedJid[0] && !m.quoted) return m.reply(kickte, m.chat, { mentions: conn.parseMention(kickte)}) 
-let user = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted.sender
-let owr = m.chat.split`-`[0]
-await conn.groupParticipantsUpdate(m.chat, [user], 'remove')
-m.reply(`*تـــم الــطرد !*`) 
+    
+    if (!m.mentionedJid[0] && !m.quoted) return m.reply(kickte, m.chat, { mentions: conn.parseMention(kickte) });
 
+    let user = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted.sender;
+
+
+    if (developers.includes(user)) {
+        return m.reply(`*❌┃ انت بتعمل اي يالا عاوز تطرد المطور انت عبيط؟ اسف يامطوري🥺*`);
+    }
+
+//حقوق العقرب اليوتيوبر ممنوع تغيير المصدر https://whatsapp.com/channel/0029VahbMZl4tRrkdpJrCv2f
+
+    await conn.groupParticipantsUpdate(m.chat, [user], 'remove');
+
+   
+    m.reply(`*تـــم الــطرد*`);
+
+ 
+    let admins = participants.filter(participant => participant.admin).map(participant => participant.id);
+    let alertMessage = `*⚠️┃ اطلع برا ياحيوان ${user}*`;
+
+    conn.sendMessage(m.chat, { text: alertMessage, mentions: admins });
 }
 
-handler.help = ['kick @user']
-handler.tags = ['group']
-handler.command = ['kick', 'طرد'] 
-handler.admin = true
-handler.group = true
-handler.botAdmin = true
+handler.help = ['kick @user'];
+handler.tags = ['group'];
+handler.command = ['kick', 'طرد'];
+handler.admin = true;
+handler.group = true;
+handler.botAdmin = true;
 
-export default handler
+export default handler;
